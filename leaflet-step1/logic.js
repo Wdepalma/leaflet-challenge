ea08ee6965e8315327333a87f954e75ff70bdb32
Wdepalma <wdepalma@verizon.net>
2021-10-02 20:26:57 -0400

    Everything but the legend !?

--- leaflet-step1/logic.js
+++ leaflet-step1/logic.js
@@ -1,40 +1,28 @@
-// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson
 
+
+//Gather data for major earthquakes around the world within the past 30 days:
 var equake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 d3.json(equake_url).then(function(quake_list){
     
 
- // Create the tile layer that will be the background of our map.
+ // Create world map barckground
  var worldmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-
-// Create a baseMaps object to hold the streetmap layer.
-// var baseMaps = {
-//   "World Map": worldmap
-// };
-
-// Create an overlayMaps object to hold the bikeStations layer.
-// var overlayMaps = {
-//   "Bike Stations": bikeStations
-// };
- // Pull the "stations" property from response.data.
+//reduce json object to features level
  var quakes = quake_list.features;
 //console.log(quakes[0])
 
-//  // Initialize an array to hold bike markers.
-//  var bikeMarkers = [];
-var quake_markers = [];
+//Display world map - arbitrary center; zoom out enough to see whole world.
 var map = L.map("map", {
     center: [40.73, -54.0059],
-    zoom: 4,
+    zoom: 2,
     layers: [worldmap]
 });
 
-//  // Loop through the stations array.
-//  for (var index = 0; index < stations.length; index++) {
-//    var station = stations[index];
+
+// Loop through features to add circles on earthquake locations (sized according to magnitude) and colored according to depth.
 for (var i = 0; i < quakes.length; i++){
     var d = quakes[i].geometry.coordinates[2]
     if(255 - d*10 < 0){
@@ -52,46 +40,7 @@ for (var i = 0; i < quakes.length; i++){
         fillOpacity: 0.7,
         radius: quakes[i].properties.mag * 50000
     }).addTo(map).bindPopup("<h1>"+title+"</h1><hr><h2>Alert: "+alert+"</h2>");
-
-    console.log(quakes[i].geometry.coordinates[0],quakes[i].geometry.coordinates[1]);
-    
-    
+   
 }
-console.log(quake_markers);
-/* var shake = L.layerGroup(quake_markers, {
-    radius: 20,
-    blur: 35
-  });
-
-  var map = L.map("map", {
-    center: [40.73, -54.0059],
-    zoom: 4,
-    layers: [worldmap, shake]
-}); */
-
-
-
-
-//createMap(L.layergroup(quake_markers));
-
-//    // For each station, create a marker, and bind a popup with the station's name.
-//    var bikeMarker = L.marker([station.lat, station.lon])
-//      .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
-//    // Add the marker to the bikeMarkers array.
-//    bikeMarkers.push(bikeMarker);
-//  }
-    
-//  // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-//  createMap(L.layerGroup(bikeMarkers));
-
-// Create the map object with options.
-
 });
-
-// Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-// L.control.layers(baseMaps, overlayMaps, {
-//   collapsed: false
-// }).addTo(map);
-
 
